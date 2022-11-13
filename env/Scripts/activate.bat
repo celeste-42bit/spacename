664c00ee8d4603e5df397f477044ded8cd1db724
ᑢᗩᘿᒪᘿSᖶᘿ <studi.jasha.gruener@gmail.com>
2022-11-13 01:05:02 +0100

    Updated with warning!

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jasha\Personal Files_LIBRARY\KSAT\SOURCE\send_your_name_to_space_SRC\send_your_name_to_space\env
+set VIRTUAL_ENV=C:\Users\studi\KSat\SOURCE\Software\spacename\env
 
 if not defined PROMPT set PROMPT=$P$G
 
