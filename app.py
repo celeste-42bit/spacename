71bf01c2fe33d1f1f1fab695fd801ecc8a4a25c1
ᑢᗩᘿᒪᘿSᖶᘿ <studi.jasha.gruener@gmail.com>
2022-11-13 02:12:57 +0100

    Changes to be committed: 	modified:   app.py 	deleted:    index.html 	deleted:    main.py 	deleted:    source.png

--- app.py
+++ app.py
@@ -5,13 +5,22 @@
 # Copyright (C) 2022 celeste-42bit
 # ------------------------------------------
 
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+import os, os.path
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")  # main page, loaded from index.html
 def index():
-    return open("index.html", "r")
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])  # Getting the name entered in the text field
+def my_form_post():
+    name = request.form['text']
+    with open("names.txt", "a+") as f:  # Mode a+ creates the file "names.txt" if it does not exist and appends everything written.
+        f.write(name + "\n")
+        f.close()
+    
 
 @app.route("/page0")
 def page0():
