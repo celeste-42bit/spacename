43aae177c3e52a85b5a71eb57138655fbc74beb6
celeste-42bit <studi.jasha.gruener@gmail.com>
2022-12-01 16:47:21 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -17,15 +17,28 @@ def index():
 @app.route("/", methods=["POST"])  # Getting the name entered in the text field
 def my_form_post():
     name = request.form['text']
-    with open("names.txt", "a+") as f:  # Mode a+ creates the file "names.txt" if it does not exist and appends everything written.
-        f.write(name + "\n")
-        f.close()
-    return render_template("done.html")
+    writeName(name)
 
 
 @app.route("/page0")
 def page0():
     return "Hello World!"
 
+
+#TODO if file doesn't exist, create it and jump to write
+#TODO if file exists, check if name already exists, if not write it
+#TODO if file exists, check if name already exists, if yes, ask if user still wants to add it
+def writeName(name):
+    with open("names.txt", "r") as f:
+        if f.readlines.__contains__(name):
+            pass #TODO Page: Name already exists, u sure u want to add it?
+        else:
+            with open("names.txt", "a+") as f:  # Mode a+ creates the file "names.txt" if it does not exist and appends everything written.
+                f.write(name + "\n")
+                f.close()
+        return render_template("done.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
