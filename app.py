b33a86a19a0df6a9dedddc37d73157240b502b57
celeste-42bit <studi.jasha.gruener@gmail.com>
2022-12-15 16:37:29 +0100

    Testing new pdf handler!

--- app.py
+++ app.py
@@ -5,12 +5,15 @@
 # Copyright (C) 2022 celeste-42bit : MIT
 # ------------------------------------------
 
-from flask import Flask, render_template, redirect, url_for, request, send_from_directory, current_app
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory, current_app, send_file
+from pdfhandler import *
 import os, os.path
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./uploads"
+ticket_writer = PdfTicketWriter('./uploads/ticket_original.pdf')
+
 # route for main page + its function
 @app.route("/")  # main page, loaded from index.html
 def index():
@@ -23,6 +26,23 @@ def my_form_post():
     name = request.form['text']
     writeName(name)
     return render_template("done.html")
+
+@app.route('/do_make_ticket', methods=['POST'])
+def make_ticket(name):
+    # you will have to figure out x and y based on your ticket layout.
+    x = 113.2
+    y = 45.0
+
+    ticket_writer.add_text(name)
+    bytes_file = ticket_writer.to_bytesio()
+    ticket_writer.reload()
+
+    return send_file(
+        bytes_file,
+        mimetype='application/pdf',
+        as_attachment=True,
+        download_name=f'ticket-{name}.pdf'
+    )
 
 
 @app.route("/uploads/<path:filename>", methods=['GET', 'POST'])
