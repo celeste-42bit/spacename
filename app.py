25dab249bd6d96a50c7f19d99e93b5f0ad55e7e8
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-01-02 18:00:46 +0100

    Security updates : Quickfix of issue #5

--- app.py
+++ app.py
@@ -6,36 +6,38 @@
 # ------------------------------------------
 
 from flask import Flask, render_template, redirect, url_for, request, send_from_directory, current_app, send_file
-from pdfhandler import *
 from PIL import Image, ImageDraw, ImageFont
 import os, os.path
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./uploads"
-ticket_writer = PdfTicketWriter('./uploads/ticket_original.pdf')
 
 # route for main page + its function
 @app.route("/")  # main page, loaded from index.html
 def index():
     return render_template("index.html")
 
+#---------------------------------------------- User input & name > names.txt
 
 # extension of route "/" which accepts names
 @app.route("/", methods=['POST'])  # Getting the name entered in the text field
 def my_form_post():
-    global name  # make name globally available
+    global name
     name = request.form['text']
     writeName(name)
     return render_template("done.html")
 
-#----------------------------------------------
+#---------------------------------------------- Ticket creation
 
 @app.route("/ticket", methods=['GET', 'POST'])
 def push_ticket():
     img = Image.open('./uploads/ticket_original.jpg')
     I1 = ImageDraw.Draw(img)
-    I1.text((1960, 600), name, font = ImageFont.truetype('./static/arial-black.ttf', 40), fill=(50, 50, 50))  # text generation and placement
+
+    # text generation and placement
+    I1.text((1960, 610), name, font = ImageFont.truetype('./static/calibri.ttf', 40), fill=(50, 50, 50))
+
     #img.show() # this line displays the image on the client right after creation
     img.save("./uploads/your-ticket.jpg")  # synchronously overwrites the "old" ticket every time a new one is created
     return send_from_directory(app.config['UPLOAD_FOLDER'], 'your-ticket.jpg', as_attachment=True)
@@ -54,24 +56,8 @@ def writeName(name):
 
 
 if __name__ == "__main__":
+# "deployed mode", posting on all interfaces, port 80, debug disabled (WARNING: Debug mode is not safe for deployment!)
     app.run(host='0.0.0.0', port=80, debug=False)
 
-
-"""dumped pdf handling thingy...
-@app.route('/do_make_ticket', methods=['GET','POST'])
-def make_ticket(name = "Blubb"):
-    # you will have to figure out x and y based on your ticket layout.
-    x = 113.2
-    y = 45.0
-
-    ticket_writer.add_text(name, x, y, 12) # (str, x, y, size)
-    bytes_file = ticket_writer.to_bytesio()
-    ticket_writer.reload()
-
-    return send_file(
-        bytes_file,
-        mimetype='application/pdf',
-        as_attachment=True,
-        download_name=f'ticket-{name}.pdf'
-    )
-"""
+# "debug mode" on localhost:5000 (debug mode allows for quick server updates, every time changes to the code are saved by [CTRL]+[S])
+    #app.run(debug=True)
