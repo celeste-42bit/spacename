fe0b359d417d6e642ba31005264f739b5dce5ba8
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-01-02 18:13:28 +0100

    enabled auto-download

--- installer.sh
+++ installer.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Downloading the neccessary files from GitHub..."
+mkdir "Celestes spacename"
+cd ./"Celestes spacename"
+git clone https://github.com/celeste-42bit/spacename
+cd ./"spacename"
 echo "Creating virtual environment 'env' using python-venv, this might take a second..."
 python -m venv env
 ./env/scripts/activate.sh
@@ -6,5 +11,5 @@ echo "Installing all neccessary components for spacename on your server!"
 python -m pip install -r ./requirements.txt
 python -m pip install --upgrade pip
 echo "Done! - Press [ENTER] to quit."
-read $0
+read $a
 exit 0
